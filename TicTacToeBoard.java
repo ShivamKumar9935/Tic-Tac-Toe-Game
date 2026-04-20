4843864a134f8e274bd102f31a14455eadad5b2b
ShivamKumar9935 <codedpilott@gmail.com>
2026-04-20 13:38:51 +0530

    Added UC1: Initialize and display board

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,29 +1,32 @@
-public class TicTacToe {
+public class TicTacToeBoard {
 
     static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
-        uc1_initializeAndDisplayBoard();
-        // uc2_playerMove();  // add later
-    }
-
-    // UC1
-    static void uc1_initializeAndDisplayBoard() {
         initializeBoard();
         printBoard();
     }
 
+    // Initialize board with '-'
     static void initializeBoard() {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
+            }
+        }
     }
 
+    // Print board properly
     static void printBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++)
-                System.out.print(board[i][j] + " ");
+        System.out.println("-------------");
+
+        for (int row = 0; row < 3; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
             System.out.println();
+            System.out.println("-------------");
         }
     }
 }
