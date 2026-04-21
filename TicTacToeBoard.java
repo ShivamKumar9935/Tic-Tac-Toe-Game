e97327cf5cf1f2d4cf2d2599c087d5e2002d4451
ShivamKumar9935 <codedpilott@gmail.com>
2026-04-21 15:02:24 +0530

    UC3: Accept User Slot Input (1–9)

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToeBoard {
 
@@ -6,16 +7,20 @@ public class TicTacToeBoard {
     static char humanSymbol;
     static char computerSymbol;
 
-    
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         tossAndAssignSymbols();
         displayTossResult();
+
+        int userSlot = getUserSlot();   
+        System.out.println("You selected slot: " + userSlot);
     }
 
-    
+    // UC2: Toss and assign symbols
     static void tossAndAssignSymbols() {
         Random rand = new Random();
-        int toss = rand.nextInt(2); // 0 or 1
+        int toss = rand.nextInt(2);
 
         if (toss == 0) {
             isHumanTurn = true;
@@ -28,7 +33,7 @@ public class TicTacToeBoard {
         }
     }
 
- 
+    // UC2: Display result
     static void displayTossResult() {
         if (isHumanTurn) {
             System.out.println("Human starts first.");
@@ -38,5 +43,21 @@ public class TicTacToeBoard {
 
         System.out.println("Human Symbol: " + humanSymbol);
         System.out.println("Computer Symbol: " + computerSymbol);
+    }
+
+    // UC3: Accept user slot input (1–9)
+    static int getUserSlot() {
+        int slot;
+
+        while (true) {
+            System.out.print("Enter a slot (1-9): ");
+            slot = sc.nextInt();
+
+            if (slot >= 1 && slot <= 9) {
+                return slot;
+            } else {
+                System.out.println("Invalid input! Enter number between 1 and 9.");
+            }
+        }
     }
 }
